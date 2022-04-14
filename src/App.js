4e59c68afa09239d6a027adaf6c82c7eb733a8d9
zhanggangbin <1738238581@qq.com>
2022-04-14 17:04:08 +0800

    init template

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import Footer from './components/Footer'
 import './App.css';
 
 export default class App extends Component {
+
+  state = {
+    todos: [
+      { id: '01', name: '吃饭', done: false },
+      { id: '02', name: '睡觉', done: true },
+      { id: '03', name: '打代码', done: false }
+    ]
+  }
   render () {
+    const { todos } = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header />
-          <List />
+          <List todos={todos} />
           <Footer />
         </div>
       </div>
